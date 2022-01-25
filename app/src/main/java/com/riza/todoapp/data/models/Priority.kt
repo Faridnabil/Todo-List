1b539ab01283c70a8b38030ca89335503eebbbed
Muhamad Azis <muhamadazis9a@gmail.com>
2022-01-25 16:12:55 +0700

    add projek

--- app/src/main/java/com/riza/todoapp/data/models/Priority.kt
+++
@@ -1,8 +0,0 @@
-package com.riza.todoapp.data.models
-//Enum (Enumeration) adalah salah satu class ,yang dapat kita gunakan, untuk memuat
-// kumpulan variable,yang telah didefinisikan menjadi tipe data konstanta.
-enum class Priority {
-    HIGH,
-    MEDIUM,
-    LOW
-}
